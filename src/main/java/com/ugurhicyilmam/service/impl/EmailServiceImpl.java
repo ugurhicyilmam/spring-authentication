bb86373217fda2e87168a65a75d3558afe14d629
ugur.hicyilmam <ugurhicyilmam@gmail.com>
2017-02-15 18:20:31 +0300

    Add email service with i18n and html templates

--- src/main/java/com/ugurhicyilmam/service/impl/EmailServiceImpl.java
+++ src/main/java/com/ugurhicyilmam/service/impl/EmailServiceImpl.java
@@ -2,43 +2,77 @@ package com.ugurhicyilmam.service.impl;
 
 import com.ugurhicyilmam.model.User;
 import com.ugurhicyilmam.service.EmailService;
+import lombok.Data;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.MailSendException;
-import org.springframework.mail.SimpleMailMessage;
+import org.springframework.context.MessageSource;
+import org.springframework.mail.MailException;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.mail.javamail.MimeMessagePreparator;
 import org.springframework.stereotype.Service;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+
+import java.util.Locale;
+
 
 @Service
 public class EmailServiceImpl implements EmailService {
 
-    private final JavaMailSender mailSender;
+    private static final Logger logger = LoggerFactory.getLogger(EmailService.class);
 
+    private final JavaMailSender mailSender;
+    private final TemplateEngine emailTemplateEngine;
+    private final MessageSource messageSource;
+
+    @SuppressWarnings("SpringJavaAutowiringInspection")
     @Autowired
-    public EmailServiceImpl(JavaMailSender mailSender) {
+    public EmailServiceImpl(JavaMailSender mailSender, TemplateEngine emailTemplateEngine, MessageSource messageSource) {
         this.mailSender = mailSender;
+        this.emailTemplateEngine = emailTemplateEngine;
+        this.messageSource = messageSource;
     }
 
     @Override
     public void sendActivationEmail(User user) {
-        String recipient = user.getEmail();
+        Locale userLocale = Locale.forLanguageTag(user.getLanguage().toString());
 
-        sendMail(createSimpleMailMessage(recipient, "Hello", "World"));
+        Email email = new Email();
+        email.setTo(user.getEmail());
+        email.setSubject(messageSource.getMessage("mail.registration.subject", null, userLocale));
+        email.setContent(processActivationTemplate(messageSource.getMessage("mail.registration.message", null, userLocale)));
+
+        sendHtmlEmail(email);
     }
 
-    private boolean sendMail(SimpleMailMessage mailMessage) {
+    private String processActivationTemplate(String message) {
+        final Context context = new Context();
+        context.setVariable("message", message);
+        return emailTemplateEngine.process("email-activation", context);
+    }
+
+    private void sendHtmlEmail(Email email) {
+        MimeMessagePreparator mimeMessagePreparator = mimeMessage -> {
+            MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage);
+            messageHelper.setFrom("ugur@hicyilmam.com");
+            messageHelper.setTo(email.getTo());
+            messageHelper.setSubject(email.getSubject());
+            messageHelper.setText(email.getContent(), true);
+        };
         try {
-            mailSender.send(mailMessage);
-        } catch (MailSendException ex) {
-            throw new RuntimeException(ex);
+            mailSender.send(mimeMessagePreparator);
+        } catch (MailException e) {
+            logger.error("E-mail could not be sent. Exception message: {]", e.getMessage());
+            logger.debug("{}", e);
         }
-        return true;
     }
 
-    private SimpleMailMessage createSimpleMailMessage(String to, String subject, String message) {
-        SimpleMailMessage mailMessage = new SimpleMailMessage();
-        mailMessage.setTo(to);
-        mailMessage.setSubject(subject);
-        mailMessage.setText(message);
-        return mailMessage;
+    @Data
+    private static class Email {
+        private String to;
+        private String subject;
+        private String content;
     }
 }
